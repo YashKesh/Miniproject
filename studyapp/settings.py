5681bf873842de19ab94069d088851b95eda0c2b
YashKesh <89805020+YashKesh@users.noreply.github.com>
2022-11-02 21:12:19 +0530

    yash commit

--- studyapp/settings.py
+++ studyapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
     'rest_framework',
+    'embed_video'
 ]
 
 MIDDLEWARE = [
